590c9df538801218c29a9a289d412d94a4522253
Marlon Xavier Delgado Ruiz <marlonxavi6@gmail.com>
2024-11-04 21:19:44 -0500

    ejercicio 5

--- ejercicio5.java
+++ ejercicio5.java
@@ -6,7 +6,7 @@ public class ejercicio5 {
         //Calcular el descuento del 10%
         var descuento = precioOriginal * 0.1;
 
-        //Totala a pagar 
+        //Totala a pagar
         var totalaPagar = precioOriginal - descuento;
 
         System.out.println("El totar a pagar con el 10% de descuento es de: " + totalaPagar + " Euros");
